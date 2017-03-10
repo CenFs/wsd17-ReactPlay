ca8fc5d8d19f903b4f83cb3dbb262f97f179e656
Jingyi Bai <baij@student.tut.fi>
2017-03-10 21:58:24 +0200

    restfuldb models.py tests.py, didn't test funcs

--- reactplay/restfuldb/models.py
+++ reactplay/restfuldb/models.py
@@ -1,3 +1,69 @@
 from django.db import models
 
-# Create your models here.
+class Users(models.Model):
+    ROLES = (
+        ('pl', 'Player'),
+        ('de', 'Developer'),
+    )
+    username = models.CharField(max_length=60)
+    password = models.CharField(max_length=60)
+    role = models.CharField(max_length=2, choices=ROLES)
+    balance = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return "{} {}".format(self.name, self.role)
+
+    def pay(self, price):
+        if self.balance >= price:
+            self.balance -= price
+            return self.save()
+        else:
+            return 0
+
+    def recharge(self, price):
+        self.balance += price
+        return self.save()
+
+    def new_pwd(self, pwd):
+        self.password = pwd
+        return self.save()
+
+
+
+class Games(models.Model):
+    name = models.CharField(max_length=255)
+    author = models.ForeignKey(Users)
+    price = models.PositiveIntegerField(default=0)
+    description = models.TextField()
+    url = models.URLField()
+
+    def __str__(self):
+        return "{} {}".format(self.name, self.author)
+
+    def new_price(self, price):
+        self.price = price
+        return self.save()
+
+
+
+
+
+class UserGame(models.Model):
+    player = models.ForeignKey(Users)
+    game = models.ForeignKey(Games)
+    # purchase_date = models.DateField()
+    purchase_date = models.DateField(auto_now_add=True)
+    score = models.IntegerField(default=0)
+    state = models.TextField()
+    # save = ??
+
+    def __str__(self):
+        return "{} {}".format(self.player, self.game)
+
+    def new_score(self, score):
+        self.score = score
+        return self.save()
+
+    def set_state(self, state):
+        self.state = state
+        return self.save()
--- reactplay/restfuldb/tests.py
+++ reactplay/restfuldb/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
+from .models import Users, Games, UserGame
+import datetime
 
-# Create your tests here.
+class ModelTestCase(TestCase):
+    fixtures = ['restfuldb.xml']
+
+    def testGettingUsers(self):
+        user = Users.objects.get(pk=1)
+        self.assertEqual(user.username, "player1", "Getting Users username")
+    def testGettingGames(self):
+        game = Games.objects.get(pk=1)
+        self.assertEqual(game.name, "game1", "Getting Games name")
+        self.assertEqual(game.author.username, "developer3", "Getting Games author")
+    def testGettingUserGame(self):
+        usergame = UserGame.objects.get(pk=2)
+        usergame1 = UserGame.objects.get(pk=1)
+        self.assertEqual(usergame.score, 120, "Getting UserGame score")
+        self.assertEqual(usergame.player.username, "player2", "Getting UserGame player")
+        self.assertEqual(usergame.game.name, "game1", "Getting UserGame game")
+        self.assertEqual(usergame.purchase_date, datetime.date(2017, 3, 10), "Getting UserGame purchase_date")
+        self.assertEqual(usergame1.purchase_date, datetime.date(2017, 3, 3), "Getting UserGame purchase_date")
+
+    def testCreatingUserGame(self):
+        Users.objects.create(username="testu", password="testu", role="de")
+        developer = Users.objects.get(username="testu")
+        player = Users.objects.get(pk=1)
+        Games.objects.create(name="testg", author=developer)
+        game = Games.objects.get(name="testg")
+        UserGame.objects.create(player=player, game=game)
+        testresult = UserGame.objects.get(player=player, game=game)
+        self.assertEqual(testresult.player.username, player.username, "Getting a just created UserGame")
