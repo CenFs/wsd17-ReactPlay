3493f23b517740bd3fecd5626955d52bc51d6862
Jingyi Bai <baij@student.tut.fi>
2017-03-10 22:12:35 +0200

    restfuldb admin.py

--- reactplay/restfuldb/admin.py
+++ reactplay/restfuldb/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Users, Games, UserGame
+
+admin.site.register(Users)
+admin.site.register(Games)
+admin.site.register(UserGame)
