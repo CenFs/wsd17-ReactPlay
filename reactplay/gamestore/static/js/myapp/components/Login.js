ed5253450f6b9d567d332d2201d7dbfaba7fdf05
enbo <northlight2015@gmail.com>
2017-03-12 00:35:42 +0200

    add bootstrap className into login component

--- reactplay/gamestore/static/js/myapp/components/Login.js
+++ reactplay/gamestore/static/js/myapp/components/Login.js
@@ -2,16 +2,25 @@ import React from 'react';
 
 const Login = () => (
 
-<form>
-  <label>
-    Name:
-    <input type="text" name="name" />
-  </label>
-  <label>
-    Password:
-    <input type="password" name="password" />
-  </label>
-  <input type="submit" value="Login" />
+<form className="login">
+  <h1> React Play </h1>
+  <div className="form-group row">
+    <label htmlFor="inputEmail" className="col-sm-2 col-form-label">Username</label>
+    <div className="col-sm-10">
+    <input type="text" name="username" className="form-control" id="inputEmail" placeholder="Email" />
+    </div>
+  </div>
+  <div className="form-group row">
+    <label htmlFor="inputPassword" className="col-sm-2 col-form-label">Password</label>
+    <div className="col-sm-10">
+    <input type="password" className="form-control" id="inputPassword" placeholder="Password" />
+    </div>
+  </div>
+  <div className="form-group row">
+    <div className="offset-sm-2 col-sm-10">
+      <button type="submit" className="btn btn-primary">Sign in</button>
+    </div>
+  </div>
 </form>
 
 );
