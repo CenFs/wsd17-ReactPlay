69ce54e2f8b4cd104ac2014b7ed82a927cc141b9
enbo <northlight2015@gmail.com>
2017-04-06 23:32:43 +0300

    add Pagination for game table

--- reactplay/gamestore/static/js/myapp/components/gameList.js
+++ reactplay/gamestore/static/js/myapp/components/gameList.js
@@ -4,7 +4,15 @@ import ReactDOM from 'react-dom';
 
 const games = [{id: 1, name: "BrowserQuest", price: 3.99, desc: "A Massively Multiplayer Adventure", url:"http://browserquest.mozilla.org/"},
 {id:2, name:"HEXGL", price:5.99, desc: "A futuristic, fast-paced racing game", url:"http://hexgl.bkcore.com/play/"},
-{id:3, name:"HEXGL 2", price:6.99, desc: "A new, fast-paced racing game", url:"http://hexgl.bkcore.com/play/"}]
+{id:3, name:"HEXGL 2", price:6.99, desc: "A new, fast-paced racing game", url:"http://hexgl.bkcore.com/play/"},
+{id:4, name:"HEXGL 3", price:6.99, desc: "A new, fast-paced racing game", url:"http://hexgl.bkcore.com/play/"},
+{id:5, name:"HEXGL 4", price:6.99, desc: "A new, fast-paced racing game", url:"http://hexgl.bkcore.com/play/"},
+{id:6, name:"HEXGL 5", price:6.99, desc: "A new, fast-paced racing game", url:"http://hexgl.bkcore.com/play/"},
+{id:7, name:"HEXGL 6", price:6.99, desc: "A new, fast-paced racing game", url:"http://hexgl.bkcore.com/play/"},
+{id:8, name:"HEXGL 7", price:6.99, desc: "A new, fast-paced racing game", url:"http://hexgl.bkcore.com/play/"},
+{id:9, name:"HEXGL 8", price:6.99, desc: "A new, fast-paced racing game", url:"http://hexgl.bkcore.com/play/"},
+{id:10, name:"HEXGL 9", price:6.99, desc: "A new, fast-paced racing game", url:"http://hexgl.bkcore.com/play/"},
+{id:11, name:"HEXGL 10", price:6.99, desc: "A new, fast-paced racing game", url:"http://hexgl.bkcore.com/play/"},]
 
 function priceFormatter (cell, row) {
   return `<i class='glyphicon glyphicon-euro'></i> ${cell}`;
@@ -14,11 +22,11 @@ class GameList extends React.Component {
   render () {
     return (
       <div>
-        <BootstrapTable data={games}>
+        <BootstrapTable data={games} pagination>
             <TableHeaderColumn dataField='id' isKey={true} >Game ID</TableHeaderColumn>
             <TableHeaderColumn dataField='name' filter={{ type: 'TextFilter', delay: 200 }}>Game Name</TableHeaderColumn>
             <TableHeaderColumn dataField='desc' >Description</TableHeaderColumn>
-            <TableHeaderColumn dataField='price' dataSort={true} dataFormat={ priceFormatter }>Game Price</TableHeaderColumn>
+            <TableHeaderColumn dataField='price' dataSort={true} dataFormat={priceFormatter}>Game Price</TableHeaderColumn>
             <TableHeaderColumn dataField='url'>URL</TableHeaderColumn>
         </BootstrapTable>
       </div>
