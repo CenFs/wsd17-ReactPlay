3cb4aa3650a70265daabf97a0f41ae3d4e3dfa2c
Pauli Tupeli <pauli.tupeli@student.tut.fi>
2017-04-11 23:00:31 +0300

    fetch genres from API

--- reactplay/gamestore/static/js/myapp/components/gameList.js
+++ reactplay/gamestore/static/js/myapp/components/gameList.js
@@ -2,6 +2,7 @@ import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { browserHistory } from 'react-router';
+import { fetchGenres } from '../actions';
 
 const games = [{id: 1, name: "BrowserQuest", price: 3.99, desc: "A Massively Multiplayer Adventure", genre:0, url:"http://browserquest.mozilla.org/"},
 {id:2, name:"HEXGL", price:5.99, desc: "A futuristic, fast-paced racing game", genre:1, url:"http://hexgl.bkcore.com/play/"},
@@ -15,13 +16,6 @@ const games = [{id: 1, name: "BrowserQuest", price: 3.99, desc: "A Massively Mul
 {id:10, name:"HEXGL 9", price:6.99, desc: "A new, fast-paced racing game", genre:2, url:"http://hexgl.bkcore.com/play/"},
 {id:11, name:"HEXGL 10", price:6.99, desc: "A new, fast-paced racing game", genre:1, url:"http://hexgl.bkcore.com/play/"},]
 
-const genreType = {
-  0: 'Platform',
-  1: 'Shooter',
-  2: 'Fighting',
-  3: 'Adventure',
-  4: 'Survival'
-};
 
 function priceFormatter (cell, row) {
   return `<i class='glyphicon glyphicon-euro'></i> ${cell}`;
@@ -47,10 +41,38 @@ function urlFormatter (cell, row, enumObject, rowIndex) {
 function enumFormatter(cell, row, enumObject) {
   return enumObject[cell];
 }
+function genreTypeFormatter(cell, row, enumObject) {
+  if (enumObject[row.genre] && enumObject[row.genre].name)
+  {
+    return enumObject[row.genre].name;
+  }
+  return cell;
+}
 // <TableHeaderColumn dataField='genre' filter={{ type: 'SelectFilter', options: genreType, condition: 'eq'}}>Game Genre</TableHeaderColumn>
 // <TableHeaderColumn dataField='genre'>Game Genre</TableHeaderColumn>
 
 class GameList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      genreTypes: {}
+    };
+  }
+  
+  componentDidMount() {
+    // Get genres for the filter
+    return fetch('/api/genres')
+      .then(x=>x.json())
+      .then(y=> {
+        var genres = {}
+        for (var i = 0; i < y.genrelist.length; ++i) {
+          // id-1 so we dont miss one genre
+          genres[y.genrelist[i].genreid-1] = y.genrelist[i].name;
+        }
+        this.setState({genreTypes:genres})
+      });
+    }
+  
   render () {
     return (
       <div>
@@ -58,8 +80,8 @@ class GameList extends React.Component {
             <TableHeaderColumn dataField='id' isKey={true} width='10%'>Game ID</TableHeaderColumn>
             <TableHeaderColumn dataField='name' filter={{ type: 'TextFilter', delay: 200 }} width='20%'>Game Name</TableHeaderColumn>
             <TableHeaderColumn dataField='desc' tdStyle={{ whiteSpace: 'normal' }}>Description</TableHeaderColumn>
-            <TableHeaderColumn dataField='genre' filterFormatted dataFormat={ enumFormatter } formatExtraData={ genreType }
-              filter={ { type: 'SelectFilter', options: genreType } }>Game Genre</TableHeaderColumn>
+            <TableHeaderColumn dataField='genre' filterFormatted dataFormat={ enumFormatter } formatExtraData={ this.state.genreTypes }
+              filter={ { type: 'SelectFilter', options: this.state.genreTypes } }>Game Genre</TableHeaderColumn>
             <TableHeaderColumn dataField='price' dataSort={true} dataFormat={priceFormatter} width='10%'>Price</TableHeaderColumn>
             <TableHeaderColumn dataField='url' dataFormat={urlFormatter}>Buy or Play</TableHeaderColumn>
         </BootstrapTable>
