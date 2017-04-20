c8633a2f7ecb3beae3c69f4354d2ebbad14f0680
EnboChen <enbo.chen@tut.fi>
2017-04-20 12:50:59 +0300

    cleaning up

--- gamestore/static/js/index.js
+++ gamestore/static/js/index.js
@@ -8,7 +8,6 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import Main from './myapp/components/Main';
 import Login from './myapp/components/Login';
-//import LoginPage from './myapp/containers/LoginPage';
 import Register from './myapp/components/Register';
 import reducer from './myapp/reducers';
 import thunk from 'redux-thunk';
@@ -19,10 +18,7 @@ import NoMatch from './myapp/components/NoMatch.js';
 import EnsureLoggedInContainer from './myapp/containers/EnsureLoggedInContainer';
 
 import { Router, Route, browserHistory } from 'react-router';
-//import createHistory from 'history/createBrowserHistory';
 import { ConnectedRouter, routerReducer, routerMiddleware, push } from 'react-router-redux';
-
-//const history = createHistory();
 
 const middleware = [ thunk ];
 
