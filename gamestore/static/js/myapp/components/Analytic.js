8bd911df730006f04a5463a123dd29a2a660e53e
enbo <northlight2015@gmail.com>
2017-04-21 22:45:26 +0300

    add analytic component

---
+++ gamestore/static/js/myapp/components/Analytic.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Analytics } from '../actions';
+
+class Analytics extends React.Component {
+
+  componentDidMount () {
+    // Fetch genres for the filter
+    // this.props.dispatch(fetchAnalytics(gameId));
+  }
+
+  render () {
+    return (
+      <div>
+       <h1> Analytics </h1>
+       <div> </div>
+      </div>
+    );
+  }
+}
+
+export default Analytics;
