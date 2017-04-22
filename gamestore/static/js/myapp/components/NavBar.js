3606f1970719cfc6efe25fcf4ce7d0327f2e77f8
enbo <northlight2015@gmail.com>
2017-04-22 22:59:21 +0300

    remove useless buttons on navBar

--- gamestore/static/js/myapp/components/NavBar.js
+++ gamestore/static/js/myapp/components/NavBar.js
@@ -25,19 +25,7 @@ class NavBar extends React.Component {
         </div>
         <ul className="nav navbar-nav">
           <li className="active"><a href="#">Home</a></li>
-          <li><a href="#">Game Store</a></li>
-          <li><a href="#">My Games</a></li>
         </ul>
-        <form className="navbar-form navbar-left">
-          <div className="input-group">
-            <input type="text" className="form-control" placeholder="Search Game" />
-            <div className="input-group-btn">
-              <button className="btn btn-default" type="submit">
-                <i className="glyphicon glyphicon-search"></i>
-              </button>
-            </div>
-          </div>
-        </form>
         <ul className="nav navbar-nav navbar-right">
           <li><a href="#"><span className="glyphicon glyphicon-user"></span> {this.props.Username} </a></li>
           <li><a href="#" onClick={this.handleLogout}><span className="glyphicon glyphicon-log-out"></span> Logout</a></li>
